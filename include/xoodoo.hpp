66f26bab0d5115c23b26af9942ab47d7e5b9b740
Anjan Roy <anjanroy@yandex.com>
2022-04-22 14:52:51 +0000

    `θ` step mapping of xoodoo permutation

---
+++ include/xoodoo.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include <bit>
+#include <cstdint>
+
+using size_t = std::size_t;
+
+// Xoodoo permutation which empowers Xoodyak cryptographic suite !
+namespace xoodoo {
+
+// Xoodoo permutation has 12 rounds; see abstract of Xoodyak specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/xoodyak-spec-final.pdf
+constexpr size_t ROUNDS = 12ul;
+
+// Xoodoo round constants; see table 2 of Xoodyak specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/xoodyak-spec-final.pdf
+constexpr uint32_t RC[ROUNDS] = { 0x00000058, 0x00000038, 0x000003c0,
+                                  0x000000d0, 0x00000120, 0x00000014,
+                                  0x00000060, 0x0000002c, 0x00000380,
+                                  0x000000f0, 0x000001a0, 0x00000012 };
+
+// Given a plane of Xoodoo permutation state ( each plane has 4 lanes, each lane
+// of 32 -bit ), this function cyclically shifts the plane such that bit at
+// position (x, z) moves to (x+t, z+v)
+//
+// Note, at z = 0 bit index, least significant bit of each lane lives !
+//
+// See row 2 of table 1 in Xoodyak specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/xoodyak-spec-final.pdf
+static inline void
+cyclic_shift(uint32_t* const plane, const size_t t, const size_t v)
+{
+#if defined(__clang__)
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    plane[i] = std::rotl(plane[i], v);
+  }
+
+  uint32_t shifted[4];
+
+#if defined(__clang__)
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    shifted[(i + t) % 4] = plane[i];
+  }
+
+#if defined(__clang__)
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    plane[i] = shifted[i];
+  }
+}
+
+// θ step mapping of Xoodoo permutation, as described in algorithm 1 of Xoodyak
+// specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/xoodyak-spec-final.pdf
+static inline void
+theta(uint32_t* const state)
+{
+  uint32_t p0[4];
+  uint32_t p1[4];
+  uint32_t e[4];
+
+#if defined(__clang__)
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    const uint32_t parity = state[i] ^ state[i + 4] ^ state[i + 8];
+
+    p0[i] = parity;
+    p1[i] = parity;
+  }
+
+  cyclic_shift(p0, 1, 5);
+  cyclic_shift(p1, 1, 14);
+
+#if defined(__clang__)
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    e[i] = p0[i] ^ p1[i];
+  }
+
+#if defined(__clang__)
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    state[i] ^= e[i];
+    state[i + 4] ^= e[i];
+    state[i + 8] ^= e[i];
+  }
+}
+
+}
