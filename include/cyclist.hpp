22dad9745c8ffa15a98bb014cd8611cc8c6d9030
Anjan Roy <hello@itzmeanjan.in>
2022-04-29 12:28:38 +0530

    cyclist mode's internal function --- `up()`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/cyclist.hpp
+++ include/cyclist.hpp
@@ -34,20 +34,20 @@ enum mode_t
 // Also see algorithmic definition in algorithm 3 of aforelinked document
 template<const mode_t m, const uint8_t color>
 static inline void
-down(uint32_t* const state,
-     const uint8_t* const blk,
+down(uint32_t* const __restrict state,
+     const uint8_t* const __restrict blk,
      const size_t b_len,
-     phase_t* const ph)
+     phase_t* const __restrict ph)
 {
   const size_t full_lane_cnt = b_len >> 2;
-  const size_t part_lane_cnt = b_len & 3ul;
+  const size_t part_lane_byt = b_len & 3ul;
 
   for (size_t i = 0; i < full_lane_cnt; i++) {
     const uint32_t lane = from_be_bytes(blk + (i << 2));
     state[i] ^= lane;
   }
 
-  if (part_lane_cnt == 3ul) {
+  if (part_lane_byt == 3ul) {
     const size_t b_off = full_lane_cnt << 2;
 
     const uint32_t lane = (static_cast<uint32_t>(blk[b_off + 0]) << 24) |
@@ -55,7 +55,7 @@ down(uint32_t* const state,
                           (static_cast<uint32_t>(blk[b_off + 2]) << 8) | 0x01u;
 
     state[full_lane_cnt] ^= lane;
-  } else if (part_lane_cnt == 2ul) {
+  } else if (part_lane_byt == 2ul) {
     const size_t b_off = full_lane_cnt << 2;
 
     const uint32_t lane = (static_cast<uint32_t>(blk[b_off + 0]) << 24) |
@@ -63,7 +63,7 @@ down(uint32_t* const state,
                           (0x01u << 8);
 
     state[full_lane_cnt] ^= lane;
-  } else if (part_lane_cnt == 1ul) {
+  } else if (part_lane_byt == 1ul) {
     const size_t b_off = full_lane_cnt << 2;
 
     const uint32_t tmp0 = (static_cast<uint32_t>(blk[b_off + 0]) << 24);
@@ -83,6 +83,56 @@ down(uint32_t* const state,
   }
 
   *ph = phase_t::Down;
+}
+
+// Internal function used in Cyclist mode of operation, which aims to produce
+// N -bytes output
+//
+// See `Inside Cyclist` in section 2.2 of Xoodyak specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/xoodyak-spec-final.pdf
+//
+// Also see algorithmic definition in algorithm 3 of aforelinked document
+template<const mode_t m, const uint8_t color>
+static inline void
+up(uint32_t* const __restrict state,
+   uint8_t* const __restrict blk,
+   const size_t b_len,
+   phase_t* const __restrict ph)
+{
+  if (m == mode_t::Keyed) {
+    state[11] ^= static_cast<uint32_t>(color);
+  }
+
+  xoodoo::permute(state);
+
+  const size_t full_lane_cnt = b_len >> 2;
+  const size_t part_lane_byt = b_len & 3ul;
+
+  for (size_t i = 0; i < full_lane_cnt; i++) {
+    to_be_bytes(state[i], blk + (i << 2));
+  }
+
+  if (part_lane_byt == 3ul) {
+    const size_t b_off = full_lane_cnt << 2;
+    const uint32_t lane = state[full_lane_cnt];
+
+    blk[b_off + 0] = static_cast<uint8_t>(lane >> 24);
+    blk[b_off + 1] = static_cast<uint8_t>(lane >> 16);
+    blk[b_off + 2] = static_cast<uint8_t>(lane >> 8);
+  } else if (part_lane_byt == 2ul) {
+    const size_t b_off = full_lane_cnt << 2;
+    const uint32_t lane = state[full_lane_cnt];
+
+    blk[b_off + 0] = static_cast<uint8_t>(lane >> 24);
+    blk[b_off + 1] = static_cast<uint8_t>(lane >> 16);
+  } else if (part_lane_byt == 1ul) {
+    const size_t b_off = full_lane_cnt << 2;
+    const uint32_t lane = state[full_lane_cnt];
+
+    blk[b_off + 0] = static_cast<uint8_t>(lane >> 24);
+  }
+
+  *ph = phase_t::Up;
 }
 
 }
--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
 
+using size_t = std::size_t;
+
 // Given four big endian bytes, this function interprets it as a 32 -bit
 // unsigned integer
 static inline uint32_t
@@ -10,4 +12,17 @@ from_be_bytes(const uint8_t* const bytes)
          (static_cast<uint32_t>(bytes[1]) << 16) |
          (static_cast<uint32_t>(bytes[2]) << 8) |
          (static_cast<uint32_t>(bytes[3]) << 0);
+}
+
+// Given a 32 -bit unsigned integer, this function interprets it as a big endian
+// byte array
+static inline void
+to_be_bytes(const uint32_t word, uint8_t* const bytes)
+{
+#if defined __clang__
+#pragma unroll 4
+#endif
+  for (size_t i = 0; i < 4; i++) {
+    bytes[i] = static_cast<uint8_t>(word >> ((3ul - i) << 3));
+  }
 }
