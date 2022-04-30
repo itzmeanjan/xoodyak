11399faffca876eb99b1a5f886d84c2614e81821
Anjan Roy <hello@itzmeanjan.in>
2022-04-30 20:32:23 +0530

    generate random N -many bytes
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <iomanip>
+#include <random>
 #include <sstream>
 
 using size_t = std::size_t;
@@ -41,4 +42,17 @@ to_hex(const uint8_t* const bytes, const size_t len)
     ss << std::setw(2) << std::setfill('0') << static_cast<uint32_t>(bytes[i]);
   }
   return ss.str();
+}
+
+// Generate `len` -many random 8 -bit unsigned integers
+static inline void
+random_data(uint8_t* const data, const size_t len)
+{
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<uint8_t> dis;
+
+  for (size_t i = 0; i < len; i++) {
+    data[i] = dis(gen);
+  }
 }
