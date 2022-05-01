a050a4ea2346d6f577e27d98d32017d620b51e91
Anjan Roy <hello@itzmeanjan.in>
2022-05-01 10:19:24 +0530

    authenticated encryption/ verified decryption routines added
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/xoodyak.hpp
+++ include/xoodyak.hpp
@@ -31,4 +31,64 @@ hash(const uint8_t* const __restrict msg, // N -bytes input message to be hashed
   cyclist::squeeze<cyclist::mode_t::Hash>(state, out, DIGEST_LEN, &ph);
 }
 
+// Xoodyak Authenticated Encryption with Associated Data routine, which given 16
+// -bytes secret key, 16 -bytes public message nonce, N -bytes associated data (
+// never encryted ) & M -bytes plain text data ( it'll be encrypted ), computes
+// M -bytes cipher text along with 16 -bytes authentication tag ( works as
+// Message Authentication Code )
+static inline void
+encrypt(const uint8_t* const __restrict key,   // 128 -bit secret key
+        const uint8_t* const __restrict nonce, // 128 -bit public message nonce
+        const uint8_t* const __restrict data, // N (>= 0) -bytes associated data
+        const size_t dt_len,                  // len(data)
+        const uint8_t* const __restrict text, // M (>=0) -bytes plain text
+        uint8_t* const __restrict cipher,     // M (>=0) -bytes cipher text
+        const size_t ct_len,                  // len(text) == len(cipher)
+        uint8_t* const __restrict tag         // 128 -bit authentication tag
+)
+{
+  cyclist::phase_t ph = cyclist::phase_t::Up;
+  uint32_t state[12] = { 0u };
+
+  cyclist::absorb_key(state, key, nonce, &ph);
+  cyclist::absorb<cyclist::mode_t::Keyed>(state, data, dt_len, &ph);
+  cyclist::encrypt(state, text, cipher, ct_len, &ph);
+  cyclist::squeeze<cyclist::mode_t::Keyed>(state, tag, 16ul, &ph);
+}
+
+// Xoodyak Verified Decryption with Associated Data routine, which given 16
+// -bytes secret key, 16 -bytes public message nonce, 16 -bytes authentication
+// tag, N -bytes associated data ( never encryted ) & M -bytes cipher text data,
+// computes M -bytes deciphered text along with boolean flag denoting
+// verification status
+//
+// Note, before consuming decrypted bytes ensure boolean flag is truth value
+static inline bool
+decrypt(const uint8_t* const __restrict key,   // 128 -bit secret key
+        const uint8_t* const __restrict nonce, // 128 -bit public message nonce
+        const uint8_t* const __restrict tag,   // 128 -bit authentication tag
+        const uint8_t* const __restrict data, // N (>= 0) -bytes associated data
+        const size_t dt_len,                  // len(data)
+        const uint8_t* const __restrict cipher, // M (>=0) -bytes cipher text
+        uint8_t* const __restrict text,         // M (>=0) -bytes plain text
+        const size_t ct_len                     // len(cipher) == len(text)
+)
+{
+  cyclist::phase_t ph = cyclist::phase_t::Up;
+  uint32_t state[12] = { 0u };
+  uint8_t tag_[16] = { 0u };
+
+  cyclist::absorb_key(state, key, nonce, &ph);
+  cyclist::absorb<cyclist::mode_t::Keyed>(state, data, dt_len, &ph);
+  cyclist::decrypt(state, cipher, text, ct_len, &ph);
+  cyclist::squeeze<cyclist::mode_t::Keyed>(state, tag_, 16ul, &ph);
+
+  bool f = false;
+  for (size_t i = 0; i < 16; i++) {
+    f |= static_cast<bool>(tag[i] ^ tag_[i]);
+  }
+
+  return !f;
+}
+
 }
