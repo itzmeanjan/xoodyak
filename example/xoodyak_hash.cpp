fae7dafc20d99cec8b32ac4e76f1213f6defd2a7
Anjan Roy <hello@itzmeanjan.in>
2022-05-02 07:37:04 +0530

    xoodyak hash C++ API usage example
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ example/xoodyak_hash.cpp
@@ -0,0 +1,28 @@
+#include "xoodyak.hpp"
+#include <iostream>
+
+// Example program demonstrating usage of Xoodyak Hash C++ API
+//
+// Compile it with
+// g++ -std=c++20 -Wall -Wextra -O3 -I ./include example/xoodyak_hash.cpp
+int
+main()
+{
+  constexpr size_t msg_len = 64ul;
+
+  uint8_t* msg = static_cast<uint8_t*>(std::malloc(msg_len));
+  uint8_t* out = static_cast<uint8_t*>(std::malloc(xoodyak::DIGEST_LEN));
+
+  random_data(msg, msg_len);
+
+  xoodyak::hash(msg, msg_len, out);
+
+  std::cout << "Message         : " << to_hex(msg, msg_len) << std::endl;
+  std::cout << "Xoodyak Digest  : " << to_hex(out, xoodyak::DIGEST_LEN)
+            << std::endl;
+
+  std::free(msg);
+  std::free(out);
+
+  return EXIT_SUCCESS;
+}
